2cbca3ade374244b0dc602b35fd3c151c89dc968
bl2i4n <bl2i4n@yahoo.com>
2017-02-03 16:34:56 -0800

    finished checkpoint 7 assignment

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -3,34 +3,50 @@
         var SongPlayer = {};
         
         var currentSong = null;
+        
+        /*
+        *@desc Buzz object audio file
+        *@type {Object}
+        */
         var currentBuzzObject = null;
         
-        
-        //play method for the function SongPlayer
-        SongPlayer.play = function(song) {
-            console.log(song);
-            if(currentSong !== song){
-                if(currentBuzzObject){
-                    currentBuzzObject.stop();
-                    currentSong.playing = null;
-                } else if (currentSong === song){
-                    if(currentBuzzObject.isPaused()){
-                        currentBuzzObject.play();
-                    }
-                }
+        /**
+        *@function setSong
+        *@desc Stops currently playing song and loads new audio file as currentBuzzObject
+        *@param {Object} song
+        */
+        var setSong = function(song) {
+            if (currentBuzzObject) {
+                currentBuzzObject.stop();
+                currentSong.playing = null;
+            }
             
-                currentBuzzObject = new buzz.sound(song.audioUrl, {
-                    formats: ['mp3'],
-                    preload: true
-             
-                });
+            currentBuzzObject = new buzz.sound(song.audioUrl, {
+               formats: ['mp3'],
+                preload: true
+            });
             
             currentSong = song;
             
-                currentBuzzObject.play();
-                song.playing = true;
-            }
         };
+        
+        var playSong = function(song){
+            currentBuzzObject.play();
+            song.playing = true; 
+        }
+        
+        //play method for the function SongPlayer
+        SongPlayer.play = function(song) {
+            //console.log(song);
+            if(currentSong !== song){
+                    setSong(song);
+                    playSong(song);
+                } else if (currentSong === song){
+                    if(currentBuzzObject.isPaused()){
+                        playSong(song);
+                    }
+                }
+            };
         
         
         SongPlayer.pause = function(song){
--- dist/scripts/controllers/AlbumCtrl.js
+++ dist/scripts/controllers/AlbumCtrl.js
@@ -12,16 +12,3 @@ function AlbumCtrl(Fixtures, SongPlayer) {
         .controller('AlbumCtrl', ['Fixtures', 'SongPlayer', AlbumCtrl]);
     
 })();
-
-/*
-for (var i=0; i<10; i++) {
-    
-}
-
-// ng-repeat="song in songs"
-for (var songIndex = 0; songIndex <= songs.length; songIndex++) {
-    var song = songs[singIndex];
-    
-    var elem = new Document.element();
-}
-*/
